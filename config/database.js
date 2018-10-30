475bd75e72a0d1b7679c5b9e17dac3acccda4f00
kestrel97 <huffazian97@gmail.com>
2018-10-29 23:57:25 -0600

    bug-fixes

--- config/database.js
+++ config/database.js
@@ -3,3 +3,4 @@ module.exports = {
   database : "mongodb://localhost:27017/meanauthapp",
   secret : "yoursecret"
 }
+  
--- config/passport.js
+++ config/passport.js
@@ -8,7 +8,8 @@ module.exports = function(passport){
   opts.jwtFromRequest = ExtractJwt.fromAuthHeaderWithScheme('jwt');
   opts.secretOrKey = config.secret;
   passport.use(new JwtStrategy(opts, (jwt_payload, done) => {
-    User.getUserById(jwt_payload._doc._id﻿, (err, user) => {
+    // console.log(jwt_payload.data._id);
+    User.getUserById(jwt_payload.data._id﻿, (err, user) => {
       if(err){
         return done(err, false);
       }
