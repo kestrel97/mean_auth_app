f238feea9eb3e5b424cd4366d79a4c7c0ff3875b
kestrel97 <huffazian97@gmail.com>
2018-11-06 22:06:27 -0700

    part-9

--- angular-src/src/app/components/profile/profile.component.ts
+++ angular-src/src/app/components/profile/profile.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+
 
 @Component({
   selector: 'app-profile',
@@ -6,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
-
-  constructor() { }
+  user: object;
+  constructor(private authService:AuthService) { }
 
   ngOnInit() {
+    this.authService.getProfile().subscribe( profile => {
+      this.user = profile.user;
+
+    },
+    err => {
+      console.log(err);
+      return false;
+    });
   }
 
 }
